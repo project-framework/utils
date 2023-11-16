46d848557a01224bcb49da1c1515adbc3fd44af6
Zhangyao719 <1258858403@qq.com>
2023-11-16 14:59:09 +0800

    wip: location type

--- packages/location/index.ts
+++ packages/location/index.ts
@@ -1,12 +1,12 @@
-import flatMap from 'lodash/flatMap';
+import { flatMap } from 'lodash-es';
 
 // 获取 location.search/hash 中的值
-export const searchParams = (search, ...keys) => {
+export const searchParams = (search: string, ...keys: string[]) => {
     search = search.replace(/#|\?/, '');
     const params = new URLSearchParams(search);
 
     return flatMap(keys).reduce((prev, key) => {
         prev[key] = params.get(key) || undefined;
         return prev;
-    }, {});
+    }, {} as {[key: string]: any});
 };
