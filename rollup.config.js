6c52327ba19760e47ed2d1bed657c4b96f01fd56
Zhangyao719 <1258858403@qq.com>
2023-11-22 18:36:30 +0800

    chore: config rollup

---
+++ rollup.config.js
@@ -0,0 +1,45 @@
+const resolve = require('@rollup/plugin-node-resolve');
+const typescript = require('@rollup/plugin-typescript');
+const commonjs = require('@rollup/plugin-commonjs');
+const terser = require('@rollup/plugin-terser');
+const postcss = require('rollup-plugin-postcss');
+
+module.exports = [
+    {
+        input: './packages/index.ts',
+        output: [
+            {
+                dir: 'lib',
+                format: 'cjs',
+                entryFileNames: '[name].cjs.js',
+                sourcemap: false, // 是否输出sourcemap
+            },
+            {
+                dir: 'lib',
+                format: 'esm',
+                entryFileNames: '[name].esm.js',
+                sourcemap: false, // 是否输出sourcemap
+            },
+            {
+                dir: 'lib',
+                format: 'umd',
+                entryFileNames: '[name].umd.js',
+                name: '$utils', // umd模块名称，相当于一个命名空间，会自动挂载到window下面
+                sourcemap: false,
+                plugins: [terser()],
+            },
+        ],
+        plugins: [
+            postcss({
+                minimize: true,
+                extensions: ['.css'],
+                extract: true,
+                // extract: path.resolve('dist/my-faceapi-js-lib.css'),
+            }),
+            resolve(),
+            commonjs(),
+            typescript({ module: 'ESNext' }),
+
+        ],
+    },
+];
