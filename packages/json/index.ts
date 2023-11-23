75986bd0d2c1d5448486a0a4961eccd05baa6440
Zhangyao719 <1258858403@qq.com>
2023-11-23 20:47:51 +0800

    test: modify location unit test

--- packages/json/index.ts
+++ packages/json/index.ts
@@ -1,12 +1,10 @@
 // 解析 JSON
-export function jsonParser(
-    text: string,
-    reviver?: (this: any, key: string, value: any) => any,
-) {
+export function jsonParser(text: string, reviver?: (this: any, key: string, value: any) => any) {
     if (typeof text === 'string') {
         try {
             return JSON.parse(text, reviver);
         } catch (error) {
+            // eslint-disable-next-line no-console
             console.error(`${text} is not a JSON`);
             return null;
         }
