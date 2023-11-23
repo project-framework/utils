2989527ac5e8f52399dae439c2a27695e06c3fcd
Zhangyao719 <1258858403@qq.com>
2023-11-23 20:47:51 +0800

    test: modify location unit test

--- packages/location/__test__/index.test.ts
+++ packages/location/__test__/index.test.ts
@@ -2,14 +2,14 @@
 import { searchParams } from '../index';
 
 describe('searchParams', () => {
-    test('should return an empty object when search is empty', () => {
+    test('should return an object with null for specified keys when search is empty', () => {
         const result = searchParams('', 'key1', 'key2');
-        expect(result).toEqual({});
+        expect(result).toEqual({ key1: null, key2: null });
     });
 
-    test('should return an empty object when search does not contain any specified keys', () => {
+    test('should return an object with null for specified keys when search does not contain any specified keys', () => {
         const result = searchParams('param1=value1&param2=value2', 'key1', 'key2');
-        expect(result).toEqual({});
+        expect(result).toEqual({ key1: null, key2: null });
     });
 
     test('should return an object with values for specified keys when search contains those keys', () => {
@@ -17,9 +17,9 @@ describe('searchParams', () => {
         expect(result).toEqual({ key1: 'value2', key2: 'value3' });
     });
 
-    test('should return undefined for keys that are not present in the search', () => {
+    test('should return null for keys that are not present in the search', () => {
         const result = searchParams('param1=value1&key1=value2', 'key1', 'key2');
-        expect(result).toEqual({ key1: 'value2', key2: undefined });
+        expect(result).toEqual({ key1: 'value2', key2: null });
     });
 
     // Add more test cases as needed
